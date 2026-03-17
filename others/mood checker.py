f7b062e97f86e949f8e51be74609a9f2418e6209
natasiaguo1412@gmail.com <natasiaguo1412@gmail.com>
2026-03-17 22:20:31 +0800

    done

--- others/mood checker.py
+++ others/mood checker.py
@@ -3,19 +3,19 @@ def prompt_mood():
     moods = {
         "happy": ["happy", "promoted", "amazing", "great", "glad", "excited", "awesome", "good"],
         "sad": ["sad", "disappointed", "upset", "unhappy", "devastated", "depressed", "down", "miserable", "heartbroken"],
-        "zangry": ["frustrated", "angry", "mad", "irritated", "pissed", "furious"],
+        "angry": ["frustrated", "angry", "mad", "irritated", "pissed", "furious"],
         "anxious": ["nervous", "exam", "anxious", "panic", "panicky", "panicked", "worried", "scared", "overthinking", "can't stop thinking","racing thoughts", "spiralling", "what if", "worried about", "heart racing", "tight chest", "short of breath", "shaking", "sweaty", "restless", "stressed", "pressure", "on edge", "uneasy", "tense"]
         }
     responses = {
         "happy": "I'm so happy to hear that!",
         "sad": "I'm so sorry to hear that, but don't worry, there will always be better days:)",
-        "zangry": "I TOTALLY UNDERSTAND HOW YOU ARE FEELING -- haizz life can be like that sometimes...",
+        "angry": "I TOTALLY UNDERSTAND HOW YOU ARE FEELING -- haizz life can be like that sometimes...",
         "anxious": "Try to take deep breaths! Breath in for 4 seconds, hold for 4 seconds, and breathe out for 4 seconds, releasing all your stress with you<3"
         } 
     scores = {
         "happy": 0,
         "sad": 0,
-        "zangry": 0,
+        "angry": 0,
         "anxious": 0
         }
     
